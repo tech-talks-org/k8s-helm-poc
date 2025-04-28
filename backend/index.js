05a4aa6a9cc88536d741657be45e58c303190709
Andrea Gernone <49447071+AndreaGern@users.noreply.github.com>
2025-04-28 20:28:07 +0300

    feat: configure helm envs for local and production (#3)

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-	res.send("Hello from Node.js backend running in Kubernetes!");
+	res.send("Hello from Node.js backend running in Kubernetes! - " + process.env.NODE_ENV);
 });
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
+
